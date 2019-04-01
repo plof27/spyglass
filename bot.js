238f2bb065e3d0e7982a6122cec7ffb8c404d779
plof27 <olaf223224@gmail.com>
2019-03-31 22:19:19 -0400

    migrate to discord.js, add sfw sasuke

--- bot.js
+++ bot.js
@@ -1,51 +1,46 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+var config = require('./config.json');
+
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
+var bot = new Discord.Client();
+
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    console.log('Connected');
+    console.log('Logged in as: ' + bot.user);
+    console.log('At: ' + bot.readyAt);
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+bot.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.content.substring(0, 1) == '!') {
+        var channel = message.channel;
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
         switch(cmd) {
-            // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+                channel.send('pong!');
             break;
             case 'bulge':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'OWO'
-                });
+                channel.send('OWO')
             break;
             case 'headpat':
-                bot.sendMessage({
-                    to: channelID,
-                    message: '>///<\n*purr*'
-                })
+                channel.send('>///<\n*purr*');
             break;
-            // Just add any case commands if you want to..
+            case 'sfw':
+                for (var i=0; i<3; i++) channel.send({
+                    files: [{
+                        attachment: './img/sasuke.jpeg',
+                        name: 'Safe for work sasuke.jpeg'
+                    }]
+                })
+                    .catch(console.log);
+            break;
          }
      }
 });
+
+bot.login(config.token);
