612b6bf751fac963acecab3016d3df095ffcfd60
plof27 <olaf223224@gmail.com>
2019-04-03 20:15:00 -0400

    add commands
    for nolan <3

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
-var Discord = require('discord.js');
+const { performance } = require('perf_hooks');
+const Discord = require('discord.js');
 require('dotenv').config();
 
 // Initialize Discord Bot
 const token = process.env.API_TOKEN || "";
-var bot = new Discord.Client();
+const bot = new Discord.Client();
+
+// state information about current race
+// This is terrible. -X
+// eat my shorts
+let race_name = "";
+let max_time = 0;
+let start_time = 0;
+let allow_join = false;
+const players = new Map();
+
+const checkpermission = function (message) {
+  return message.member.roles.find(r => r.name === "Organizer" || r.name === "Admin");
+};
+
+const process_args = string => string.substring(1).match(/\w+|"[^"]+"/g).map(x => x.replace(new RegExp('"', 'g'), ''));
 
 bot.on('ready', function (evt) {
     console.log('Connected');
@@ -16,23 +32,51 @@ bot.on('message', message => {
     // It will listen for messages that will start with `!`
     if (message.content.substring(0, 1) == '.') {
         var channel = message.channel;
-        var args = message.content.substring(1).split(' ');
-        var cmd = args[0];
+        const args = process_args(message.content);
 
-        args = args.splice(1);
-        switch(cmd) {
+        switch(args[0]) {
             case 'ping':
                 channel.send('pong!');
             break;
             case 'sfw':
+              if (checkpermission(message)) {
                 for (var i=0; i<3; i++) channel.send({
-                    files: [{
-                        attachment: './img/sasuke.jpeg',
-                        name: 'Safe for work sasuke.jpeg'
-                    }]
+                  files: [{
+                    attachment: './img/sasuke.jpeg',
+                    name: 'Safe for work sasuke.jpeg'
+                  }]
                 })
-                    .catch(console.log);
+                .catch(console.log);
+              }
             break;
+            case 'race':
+              if (checkpermission(message)) {
+                race_name = args[1]
+                max_time = args[2]
+                allow_join = true;
+                channel.send(`**${race_name}** is now open!\nTime limit: ${max_time} minutes.\nThe race will begin once all participants are ready.`);
+                if (!(players.has(message.author.id))) players.set(message.author.id, {user: message.author, ready: false});
+              } else {
+                channel.send('You do not have permission to use this command.');
+              }
+            break;
+            case 'join':
+              if (race_name !== '') {
+                if (!(players.has(message.author.id))) {
+                  players.set(message.author.id, {user: message.author, ready: false});
+                  channel.send(`${message.author} has joined the race.`);
+                } else {
+                  channel.send('You are already in the race.');
+                }
+              }
+            break;
+            case 'quit':
+              if players.delete(message.author.id) channel.send(`${message.author} has left the race.`);
+            break;
+            case 'ready':
+              if (players.has(message.author.id)) {
+                players.get(message.author.id).ready = true;
+              }
          }
      }
 });
